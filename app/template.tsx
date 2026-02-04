76d9740e1093d00885fb141ed18cda4d24d3bfa3
kaungmyatkyawtiti <eucalyptustiti2003@gmail.com>
2026-02-04 17:00:21 +0630

    feat(template): add page transition animation

---
+++ app/template.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { ReactNode } from "react";
+
+export default function Template({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  return (
+    <motion.div
+      initial={{ y: 10, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      exit={{ y: 10, opacity: 0 }}
+      transition={{
+        type: "spring",
+        stiffness: 120,
+        damping: 18,
+        mass: 0.8,
+      }}
+    >
+      {children}
+    </motion.div>
+  )
+}
+
