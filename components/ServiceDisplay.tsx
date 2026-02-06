b2789296e5fecbc13d7e8520bc3eba7884c7e667
kaungmyatkyawtiti <eucalyptustiti2003@gmail.com>
2026-02-07 02:48:22 +0630

    chore(ui): adding display section for service page

---
+++ components/ServiceDisplay.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { DUMMY_LATEST } from "@/constants";
+import { useRef } from "react";
+import { useScroll } from "motion/react";
+import ShowcaseCard from "./ShowcaseCard";
+
+const ServiceDisplay = () => {
+  const container = useRef(null);
+
+  const { scrollYProgress } = useScroll({
+    target: container,
+    offset: ["start end", "end end"],
+  });
+
+  return (
+    <section
+      ref={container}
+      className='relative px-4'
+    >
+      {DUMMY_LATEST.map((project, i) => {
+        const targetScale = 1 - ((DUMMY_LATEST.length - i) * 0.05);
+
+        return (
+          <ShowcaseCard
+            key={project.id}
+            project={project}
+            i={i}
+            progress={scrollYProgress}
+            range={[i * .25, 1]}
+            targetScale={targetScale}
+          />
+        )
+      })}
+    </section>
+  );
+};
+
+export default ServiceDisplay;
