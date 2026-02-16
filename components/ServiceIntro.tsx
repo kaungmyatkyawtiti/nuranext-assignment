6acc898b98762ddcd8234a1abe460f6ec672cc4c
kaungmyatkyawtiti <eucalyptustiti2003@gmail.com>
2026-02-16 18:59:42 +0630

    feat: adding parallex effect in service intro page

--- components/ServiceIntro.tsx
+++ components/ServiceIntro.tsx
@@ -1,14 +1,40 @@
+"use client";
+
+import Image from "next/image";
+import { motion, useScroll, useTransform, useSpring } from "motion/react";
+
 export default function ServiceIntro() {
+  const { scrollY } = useScroll();
+
+  const y = useTransform(scrollY, [0, 500], [0, 200]);
+
+  const smoothY = useSpring(y, { stiffness: 100, damping: 20 });
+
   return (
-    <section className="min-h-screen col-center overflow-hidden section-container">
-      {/* Text Content */}
-      <div className="text-center max-w-7xl">
-        <h2 className="text-[clamp(2rem,8vw,6rem)] leading-tight font-semibold">BOOST YOUR</h2>
-        <h2 className="text-[clamp(2rem,8vw,6rem)] text-brand-primary leading-tight font-semibold">
+    <section className="relative min-h-screen overflow-hidden section-container col-center">
+      <motion.div
+        className="absolute inset-0"
+        style={{ y: smoothY }}
+      >
+        <Image
+          src="/parallex.png"
+          alt="parallax"
+          fill
+          className="object-cover object-[75%_30%]"
+        />
+      </motion.div>
+
+      <div className="absolute inset-0 z-5 bg-black/45" />
+
+      <div className="relative z-10 text-center">
+        <h2 className="text-[clamp(3rem,6vw,5rem)] font-semibold text-white">
+          BOOST YOUR
+        </h2>
+        <h2 className="text-[clamp(3.5rem,6vw,5rem)] text-cyan-500 text-shadow-black font-semibold">
           GROWTH
         </h2>
       </div>
     </section>
-  )
+  );
 }
 
